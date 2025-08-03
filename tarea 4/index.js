6e60d793682999493242f085501d1fbbce4bd2f7
br1an17 <brianzairpaez@gmail.com>
2025-08-03 19:52:38 -0300

    hacemos los ejercicios 6,7,8,9,10 de la tarea 4

--- tarea 4/index.js
+++ tarea 4/index.js
@@ -4,41 +4,74 @@ console.log("¡Bienvenido al programa!");
 
 // Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.
 
-// let nombre = "Brian"
+let names = "Brian";
 
-// console.log("¡Hola " + nombre);
+console.log("¡Hola " + names + "!");
 
 // Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza alert para mostrar el mensaje "¡Hola, [tu nombre]!".
 
-
-let nombre = "Brian"
+let nombre = "Brian";
 
 alert("¡Hola " + nombre);
 
-
 // Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
 
-let lenguaje =prompt("¿Cuál es el lenguaje de programación que más te gusta?")
+let lenguaje = prompt("¿Cuál es el lenguaje de programación que más te gusta?");
 
-console.log(lenguaje)
+console.log(lenguaje);
 
 // Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la suma de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.
 
-let valor1 = 3
-let valor2 = 7
-let resultado = valor1 + valor2
-console.log(resultado)
-
+let num1 = 3;
+let num2 = 7;
+let suma = valor1 + valor2;
+console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}.`);
 
 // Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la resta de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La diferencia entre [valor1] y [valor2] es igual a [resultado]." en la consola.
 
+let valor1 = 10;
+let valor2 = 7;
+let resultado = valor1 - valor2;
+console.log(
+  "La diferencia entre" + valor1 + "y" + valor2 + "es igual a" + resultado + "."
+);
+
 // Pide al usuario que ingrese su edad con prompt. Con base en la edad ingresada, utiliza un if para verificar si la persona es mayor o menor de edad y muestra un mensaje apropiado en la consola.
+
+let edad = prompt("¿Cuál es tu edad?");
+
+if (edad > 18) {
+  console.log("Eres mayor de edad.");
+}
 
 // Crea una variable "numero" y solicita un valor con prompt. Luego, verifica si es positivo, negativo o cero utilizando un if-else y muestra el mensaje correspondiente.
 
+let numero = prompt("Ingresa un número:");
+if (numero > 0) {
+  console.log("El número es positivo.");
+} else if (numero < 0) {
+  console.log("El número es negativo.");
+} else {
+  console.log("El número es cero.");
+}
+
 // Utiliza un bucle while para mostrar los números del 1 al 10 en la consola.
 
+let n = 1
+while(n <=10){
+    console.log(n)
+    n = n + 1
+}
+
 // Crea una variable "nota" y asígnale un valor numérico. Utiliza un if-else para determinar si la nota es mayor o igual a 7 y muestra "Aprobado" o "Reprobado" en la consola.
+
+let nota = 8;
+if (nota >= 7) {  
+  console.log("Aprobado");
+}
+else {
+  console.log("Reprobado");
+}
 
 // Utiliza Math.random para generar cualquier número aleatorio y muestra ese número en la consola.
 
