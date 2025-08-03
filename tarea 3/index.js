2101d855b1c9f853a9a91c6e90a8f7776cba0b85
br1an17 <brianzairpaez@gmail.com>
2025-08-03 18:01:03 -0300

    hacemos los ejercicios 1,2,3 de la tarea 3

---
+++ tarea 3/index.js
@@ -0,0 +1,30 @@
+// Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+
+// let numero = 1;
+
+// while(numero <=10){
+//     console.log(numero)
+//     numero = numero + 1
+// }
+// Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+
+// let numero = 10;
+
+// while(numero >=0){
+//     console.log(numero)
+//     numero = numero - 1
+// }
+
+// Crea un programa de cuenta regresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+
+let numero = prompt("ingrese un numero para la cuenta regresiva")
+
+while(numero >=0){
+    console.log(numero)
+    numero = numero - 1
+}
+
+// Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+
+
+
