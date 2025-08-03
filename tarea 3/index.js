a6d22a46725d58d800cb758a229e07bb3ddfc363
br1an17 <brianzairpaez@gmail.com>
2025-08-03 18:08:40 -0300

    hacemos el ejercicio 4 de la tarea 3

--- tarea 3/index.js
+++ tarea 3/index.js
@@ -1,19 +1,19 @@
 // Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
 
-// let numero = 1;
+let n = 1;
 
-// while(numero <=10){
-//     console.log(numero)
-//     numero = numero + 1
-// }
+while(n <=10){
+    console.log(n)
+    n = n + 1
+}
 // Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
 
-// let numero = 10;
+let inicio = 10;
 
-// while(numero >=0){
-//     console.log(numero)
-//     numero = numero - 1
-// }
+while(inicio >=0){
+    console.log(inicio)
+    inicio = inicio - 1
+}
 
 // Crea un programa de cuenta regresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
 
@@ -26,5 +26,11 @@ while(numero >=0){
 
 // Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
 
+let num = prompt("ingrese un numero para la cuenta progresiva");
+numero = parseInt(numero);
 
-
+let cero = 0;
+while (cero <= num) {
+  console.log(cero);
+  cero += 1;
+}
