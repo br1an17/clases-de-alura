3f164a632b4101ebbd4bc67de7ef74e6673f5b0d
br1an17 <brianzairpaez@gmail.com>
2025-08-05 15:19:48 -0300

    unimos la funcionalidad del boton con el input para ya poder jugar

--- logica-programacion-2-ProyectoInicial/app.js
+++ logica-programacion-2-ProyectoInicial/app.js
@@ -1,11 +1,28 @@
+let numeroSecreto = generarNumeroSecreto();
 
-let textoMutable = (elemento,texto) => {
-document.querySelector(elemento).innerHTML = texto;    
+let textoMutable = (elemento, texto) => {
+  document.querySelector(elemento).innerHTML = texto;
+};
+
+function elegirNumero() {
+  let numeroElegido = parseInt(document.getElementById("numeroElegido").value);
+  return numeroElegido;
 }
 
-let inicio = () => {
-alert("El juego ha comenzado");
+function generarNumeroSecreto() {
+  return Math.floor(Math.random() * 10) + 1;
 }
 
-textoMutable("h1","juego de adivinar el numero"); 
-textoMutable("p","Elije un numero entre 1 y 10"); 
+function adivinarNumero() {
+  let numeroElegido = elegirNumero();
+  if (numeroElegido === numeroSecreto) {
+    alert("Felicidades, has adivinado el numero");
+  } else {
+    alert("Lo siento, intenta de nuevo");
+    textoMutable("p", "Elije otro numero entre 1 y 10");
+    document.getElementById("numeroElegido").value = "";
+  }
+}
+
+textoMutable("h1", "juego de adivinar el numero");
+textoMutable("p", "Elije un numero entre 1 y 10");
