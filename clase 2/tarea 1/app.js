300f80bc7ef0aa82e05901134c65895a16b9eadc
br1an17 <brianzairpaez@gmail.com>
2025-08-05 11:51:26 -0300

    terminamos los ejercicios de la primera tarea

--- clase 2/tarea 1/app.js
+++ clase 2/tarea 1/app.js
@@ -2,24 +2,33 @@
 
 // Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".
 
-let titulo = document.querySelector('h1');
+let titulo = document.querySelector("h1");
 titulo.innerHTML = "Hora del desafio";
 
 // Crea una función que muestre en la consola el mensaje "El botón fue clicado" siempre que se presione el botón "Console".
 
-let cliquiado = () =>{
-    console.log("El boton due cliqueado")
-}
+let cliquiado = () => {
+  console.log("El boton due cliqueado");
+};
 
 // Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
 
-let promp = () =>{
-    let ciudad = prompt("Dime una siudad de Brasil")
-    alert(`Estuve en ${ciudad} y me acordé de ti`)
-}
+let promp = () => {
+  let ciudad = prompt("Dime una siudad de Brasil");
+  alert(`Estuve en ${ciudad} y me acordé de ti`);
+};
 
 // Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".
 
+let alerta = () => {
+  alert("Yo amo JS");
+};
 
 // Al hacer clic en el botón "suma", pide 2 números y muestra el resultado de la suma en una alerta.
 
+let suma = () => {
+  let a = parseInt(prompt("ingrese el primer numero"));
+  let b = parseInt(prompt("ingrese un segundo numero para sumar"));
+
+  alert(a + b);
+};
