ec878e63cdb176faa83791395046954bb01fd053
br1an17 <brianzairpaez@gmail.com>
2025-08-05 14:09:20 -0300

    refactorisamos el codigo

--- logica-programacion-2-ProyectoInicial/app.js
+++ logica-programacion-2-ProyectoInicial/app.js
@@ -1,9 +1,11 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "juego de adivinar el numero";
 
-let parrafo = document.querySelector("p");
-parrafo.innerHTML = "Adivina un numero entre 1 y 10";
+let textoMutable = (elemento,texto) => {
+document.querySelector(elemento).innerHTML = texto;    
+}
 
 let inicio = () => {
 alert("El juego ha comenzado");
 }
+
+textoMutable("h1","juego de adivinar el numero"); 
+textoMutable("p","Elije un numero entre 1 y 10"); 
