91c2defac7bd99b395458cba71b14c7242f7c9d7
br1an17 <brianzairpaez@gmail.com>
2025-08-05 17:23:14 -0300

    refactorisamos el codigo

--- logica-programacion-2-ProyectoInicial/app.js
+++ logica-programacion-2-ProyectoInicial/app.js
@@ -1,17 +1,19 @@
-let numeroSecreto = generarNumeroSecreto();
 let intentos = 1;
+let input = document.getElementById("numeroElegido");
+let reinicio = document.getElementById("reiniciar");
+let numeroSecreto = generarNumeroSecreto();
 
 let textoMutable = (elemento, texto) => {
   document.querySelector(elemento).innerHTML = texto;
 };
 
 function elegirNumero() {
-  let numeroElegido = parseInt(document.getElementById("numeroElegido").value);
+  let numeroElegido = parseInt(input.value);
   return numeroElegido;
 }
 
 function generarNumeroSecreto() {
-  document.getElementById("reiniciar").disabled = true;
+  reinicio.disabled = true;
   return Math.floor(Math.random() * 10) + 1;
 }
 
@@ -25,12 +27,12 @@ function adivinarNumero() {
       }`
     );
     textoMutable("p", "El numero secreto era: " + numeroSecreto);
-    document.getElementById("numeroElegido").disabled = true;
-    document.getElementById("reiniciar").disabled = false;
+    input.disabled = true;
+    reinicio.disabled = false;
   } else {
     intentos++;
     textoMutable("h1", "Lo siento, no adivinaste el numero secreto");
-    document.getElementById("numeroElegido").value = "";
+    input.value = "";
     if (numeroElegido > numeroSecreto) {
       textoMutable("p", "El numero secreto es menor que " + numeroElegido);
     } else {
@@ -41,11 +43,11 @@ function adivinarNumero() {
 function reiniciar() {
   numeroSecreto = generarNumeroSecreto();
   intentos = 1;
-  textoMutable("h1", "juego de adivinar el numero");
-  textoMutable("p", "Elije un numero entre 1 y 10");
-  document.getElementById("numeroElegido").disabled = false;
-  document.getElementById("numeroElegido").value = "";
+  textoMutable("h1", "Juego de adivinar el número");
+  textoMutable("p", "Elige un número entre 1 y 10");
+  input.disabled = false;
+  input.value = "";
 }
 
-textoMutable("h1", "juego de adivinar el numero");
-textoMutable("p", "Elije un numero entre 1 y 10");
+textoMutable("h1", "Juego de adivinar el número");
+textoMutable("p", "Elige un número entre 1 y 10");
