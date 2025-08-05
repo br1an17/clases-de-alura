34ae16f99928e497e08eaebe4f574c32ed7c0124
br1an17 <brianzairpaez@gmail.com>
2025-08-05 11:37:26 -0300

    agregamos un alert al precionar un boton

--- logica-programacion-2-ProyectoInicial/app.js
+++ logica-programacion-2-ProyectoInicial/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = "juego de adivinar el numero";
 
 let parrafo = document.querySelector("p");
 parrafo.innerHTML = "Adivina un numero entre 1 y 10";
+
+let inicio = () => {
+alert("El juego ha comenzado");
+}
