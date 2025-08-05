19e65def34e15811e2525337491091bbb328cec5
br1an17 <brianzairpaez@gmail.com>
2025-08-04 21:41:49 -0300

    agregamos js

---
+++ logica-programacion-2-ProyectoInicial/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "juego de adivinar el numero";
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Adivina un numero entre 1 y 10";
