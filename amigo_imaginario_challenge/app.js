36947a61bd8875f90355f9c5ab14d1642f8223b0
br1an17 <brianzairpaez@gmail.com>
2025-08-12 17:33:39 -0300

    cambiamos el nombre de la carpeta y arrancamos

--- amigo_imaginario_challenge/app.js
+++ amigo_imaginario_challenge/app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+const agregarAmigo = (params) => {
+    alert("añadir")
+}
+
+const sortearAmigo = (params) => {
+    alert("sortear")
+}
