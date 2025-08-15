9d05614b4ca13be48188281717b4935d1e9eeb06
br1an17 <brianzairpaez@gmail.com>
2025-08-15 20:42:19 -0300

    otra validacion para los nombres

--- amigo_imaginario_challenge/app.js
+++ amigo_imaginario_challenge/app.js
@@ -5,18 +5,26 @@ let amigos = [];
 let textoMutable = (elemento, texto) => {
   document.querySelector(elemento).innerHTML = texto;
 };
+const ami = document.getElementById("amigo");
 
 const agregarAmigo = () => {
-  if (document.getElementById("amigo").value === "") {
+  if (ami.value === "") {
     textoMutable("h2", "Por favor, ingresa un nombre de amigo");
     return;
-  }else {
+  } else if (ami.value.length < 3 || ami.value.length > 20) {
+    textoMutable(
+      "h2",
+      "El nombre del amigo debe tener entre 3 y 20 caracteres"
+    );
+    return;
+  } else {
     textoMutable("h2", "Amigo agregado correctamente");
-  let amigo = document.getElementById("amigo").value;
-  amigos.push(amigo);
-  document.getElementById("amigo").value = "";
-  return amigos;
-};}
+    let amigo = ami.value;
+    amigos.push(amigo);
+    ami.value = "";
+    return amigos;
+  }
+};
 
 const sortearAmigo = () => {
   let num = Math.floor(Math.random() * amigos.length);
